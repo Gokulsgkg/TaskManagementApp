5979f317e9cb60c1185d68837be0b9985cacb3ed
Gokulsgkg <gokulmaniyan932005@gmail.copm>
2026-03-11 17:20:27 +0530

    Updated

--- TaskManagementApp/Models/TaskItem.cs
+++ TaskManagementApp/Models/TaskItem.cs
@@ -1,10 +1,17 @@
-﻿namespace TaskManagementApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementApp.Models
 {
     public class TaskItem
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter a title.")]
+        [StringLength(100,ErrorMessage = "The title cannot exceed 100 characters.")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter a description.")]
+        [StringLength(500,ErrorMessage = "the description cannot exceed 500 characters.")]
         public string Description { get; set; }
+        [Display(Name = "Completed")]
         public bool IsCompleted { get; set; }
     }
 }
