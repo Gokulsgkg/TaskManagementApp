1c18fdc316aea72fe2a4e1cb3ddcadcaa57d5d8a
Gokulsgkg <gokulmaniyan932005@gmail.com>
2026-03-16 15:24:14 +0530

    1603 Updates

--- TaskManagementApp/Program.cs
+++ TaskManagementApp/Program.cs
@@ -6,7 +6,7 @@ namespace TaskManagementApp
     public class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container. 
