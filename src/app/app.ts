17bc0a1c30d097d4899821bc574910e3033ebb9b
Gokulsgkg <gokulmaniyan932005@gmail.com>
2026-03-23 17:26:16 +0530

    2303

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule, UpperCasePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule,       // gives you *ngIf and *ngFor
+    UpperCasePipe,      // enables | uppercase
+    TitleCasePipe       // enables | titlecase
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('Gokul'); 
-} 
+  protected readonly title = signal('Techzines');
+
+  email: string = '';
+  status: string = '';
+  subscribers: string[] = [];
+
+  onSubmit(): void {
+    this.status = `Confirmation mail has been sent to ${this.email} successfully`;
+    this.subscribers.push(this.email);
+    this.email = ''; // clear input after submit
+  }
+}
